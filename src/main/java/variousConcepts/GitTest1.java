cd419ed5ca8f9b4e639483c6f4c96943215542b5
AbdullahObaid786 <Abdullah.obaid2027@outlook.com>
2022-03-23 23:56:08 -0500

    New Comment

---
+++ src/main/java/variousConcepts/GitTest1.java
@@ -0,0 +1,10 @@
+package variousConcepts;
+
+public class GitTest1 {
+	public static void main(String[] args) {
+		System.out.println("Hello! ");
+		System.out.println("This is World ");
+		System.out.println("QA Automation Engineer");
+	}
+
+}
